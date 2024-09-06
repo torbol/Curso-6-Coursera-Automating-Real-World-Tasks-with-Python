26dcad2d26d61fcb0a3c720b35beb52993d2572b
José Ángel <legnaes@gmail.com>
2024-09-06 03:04:21 +0200

    Se modifica el diccionario a key: value, un diccionario, un valor por cada archivo para cada request

--- Semana 2/run.py
+++ Semana 2/run.py
@@ -15,43 +15,48 @@ def list_all_txt(feedbackpath_f):
     feedback_list_names_f.sort() #Ordenamos por numeración ascendente los comentarios
     print(feedback_list_names_f) #BORRAR CUANDO TERMINE
     return feedback_list_names_f
+#TO DO LIST Crear función para hacer request
 def read_every_single_file_to_dictionary(feedbackpath_f2, feedback_list_names_f2):
     """Recibe una lista de archivos por parámetro, 
     se leerá cada archivo y se procesará la información 
     como un diccionario Python con la siguiente estructura:
 
     {
-        title:      [titulo1,       titulo2,        titulo3...]
-        name:       [nombre1,       nombre2,        nombre3...]
-        date:       [fecha1,        fecha2,         fecha3...]
-        feedback:   [comentario1,   comentario2,    comentario3...]
+        title:      "título"
+        name:       "nombre"
+        date:       "fecha"
+        feedback:   "comentario"
     }
 
     """
     #Creamos un diccionario local para la función
     dictionary = {
-        "title": [],
-        "name": [],
-        "date": [],
-        "feedback": []
+        "title": "",
+        "name": "",
+        "date": "",
+        "feedback": ""
     }
     #Abrimos cada archivo y vamos añadiendo línea por línea a cada clave: valor (key: value)
     for element in feedback_list_names_f2:
         with open(os.path.join(feedbackpath_f2, element), 'r') as file:
             text_lines_list = file.readlines() # Guardamos las líneas del archivo en una lista de forma [title, name, date, feedback]
-            #print(text_lines_list) #BORRAR CUANDO TERMINE
+
             #Añadimos los valores a cada key del diccionario iterando sobre cada cada elemento de la lista text_lines_list
             for index in range(0, len(text_lines_list)):
                 if index == 0:                    
-                    dictionary["title"].append(text_lines_list[index])
+                    dictionary["title"] = text_lines_list[index][:-1]   # Con el [:-1] final indicamos que borre los dos últimos caracteres "\n" que son los saltos de carro del final de cada línea.
                 elif index == 1:
-                    dictionary["name"].append(text_lines_list[index])
+                    dictionary["name"] = text_lines_list[index][:-1]
                 elif index == 2:
-                    dictionary["date"].append(text_lines_list[index])
+                    dictionary["date"] = text_lines_list[index][:-1]
                 elif index == 3:
-                    dictionary["feedback"].append(text_lines_list[index])    
-    print(dictionary.values()) #BORRAR CUANDO TERMINE
-
+                    dictionary["feedback"] = text_lines_list[index][:-1]
+            print(dictionary.values()) #BORRAR CUANDO TERMINE
+            
+            
+            
+            
+            #TO DO LIST llamar a la función para hacer request
 
 
 
